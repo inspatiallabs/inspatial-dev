3e3bb3f52b10d43f31eb25adfcc2a936cfcf64bb
Ben Emma <emmaben676@gmail.com>
2025-04-29 20:24:22 +0100

    chore(a11y): Rename Attributes interface and Role type for clarity
    - Renamed the Attributes interface to A11YType to better reflect its purpose in accessibility.
    - Updated the Role type to A11YRole for consistency with the new naming convention.
    - Ensured that the changes align with WAI-ARIA specifications for improved developer understanding.

--- packages/core/dev/a11y/src/type/index.ts
+++ packages/core/dev/a11y/src/type/index.ts
@@ -1,5 +1,5 @@
 // All the WAI-ARIA 1.1 attributes from https://www.w3.org/TR/wai-aria-1.1/
-export interface Attributes {
+export interface A11YType {
   /** Identifies the currently active element when DOM focus is on a composite widget, textbox, group, or application. */
   "aria-activedescendant"?: string;
   /** Indicates whether assistive technologies will present all, or only parts of, the changed region based on the change notifications defined by the aria-relevant attribute. */
@@ -247,7 +247,7 @@ export interface Attributes {
 }
 
 // All the WAI-ARIA 1.2 role attribute values from https://www.w3.org/TR/wai-aria-1.2/#role_definitions
-export type Role =
+export type A11YRole =
   | "alert"
   | "alertdialog"
   | "application"
